cb5f0979fa2f7d4f3e00f3508e436bdfba27db9d
Tran <khomson7@gmail.com>
2020-11-21 18:17:50 +0700

    edit

--- services/opitemrece.service.js
+++ services/opitemrece.service.js
@@ -4,12 +4,13 @@ module.exports = {
         pool.query(`select md5(concat('wsc',t.hospcode,t.vn,t.icode)) as mkey,t.* FROM
         (select (select hospitalcode from opdconfig) as hospcode,o.icode,d.name,o.vn
         ,md5(concat('wsc',pt.cid)) as mpid,o.qty,date_format(o.vstdate,'%Y-%m-%d') as vstdate
+        ,o.vsttime
         from opitemrece o
         INNER JOIN drugitems d on d.icode = o.icode
         LEFT JOIN patient pt on pt.hn = o.hn
         WHERE o.vstdate in(select date_work FROM wsc_t_work_pcu) 
         )t
-        where  md5(concat('wsc',t.hospcode,t.vn,t.icode)) not in(select mkey from wsc_opitemrece_pcu_check)
+        where  md5(concat('wsc',t.hospcode,t.vn,t.icode)) NOT in(select mkey from wsc_opitemrece_pcu_check)
         ORDER BY t.vn
         `,
             [],
